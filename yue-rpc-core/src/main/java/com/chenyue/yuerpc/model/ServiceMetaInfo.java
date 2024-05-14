028407879d9313fe8a92c3663178ae1102ec555f
chenyue <2511006757@qq.com>
2024-05-14 15:35:25 +0800

    新增Etcd注册中心

--- yue-rpc-core/src/main/java/com/chenyue/yuerpc/model/ServiceMetaInfo.java
+++ yue-rpc-core/src/main/java/com/chenyue/yuerpc/model/ServiceMetaInfo.java
@@ -1,5 +1,6 @@
 package com.chenyue.yuerpc.model;
 
+import cn.hutool.core.util.StrUtil;
 import com.chenyue.yuerpc.constant.RpcConstant;
 import lombok.Data;
 
@@ -18,4 +19,47 @@ public class ServiceMetaInfo {
      * 服务版本号
      */
     private String serviceVersion = RpcConstant.DEFAULT_SERVICE_VERSION;
+    /**
+     * 服务地址
+     */
+    private String serviceHost;
+    /**
+     * 服务端口
+     */
+    private Integer servicePort;
+    /**
+     * 服务分组（暂未实现）
+     */
+    private String serviceGroup = "default";
+
+    /**
+     * 获得服务键名
+     *
+     * @return
+     */
+    public String getServiceKey() {
+        return String.format("%s:%s", serviceName, serviceVersion);
+    }
+
+    /**
+     * 获取服务注册节点键名
+     *
+     * @return
+     */
+    public String getServiceNodeKey() {
+        return String.format("%s/%s:%s", getServiceKey(), serviceHost, servicePort);
+    }
+
+    /**
+     * 获取完整的服务地址
+     *
+     * @return
+     */
+    public String getServiceAddress() {
+        if (!StrUtil.contains(serviceHost, "http")) {
+            return String.format("http://%s:%s", serviceHost, servicePort);
+        }
+        return String.format("%s:%s", serviceHost, servicePort);
+    }
+
 }
--- yue-rpc-core/src/main/java/com/chenyue/yuerpc/registry/EtcdRegistry.java
+++ yue-rpc-core/src/main/java/com/chenyue/yuerpc/registry/EtcdRegistry.java
@@ -1,14 +1,23 @@
 package com.chenyue.yuerpc.registry;
 
+import cn.hutool.core.collection.CollUtil;
+import cn.hutool.cron.CronUtil;
+import cn.hutool.cron.task.Task;
+import cn.hutool.json.JSONUtil;
 import com.chenyue.yuerpc.config.RegistryConfig;
 import com.chenyue.yuerpc.model.ServiceMetaInfo;
-import io.etcd.jetcd.Client;
-import io.etcd.jetcd.KV;
+import io.etcd.jetcd.*;
+import io.etcd.jetcd.options.GetOption;
+import io.etcd.jetcd.options.PutOption;
+import io.etcd.jetcd.watch.WatchEvent;
 import io.vertx.core.impl.ConcurrentHashSet;
 
+import java.nio.charset.StandardCharsets;
+import java.time.Duration;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * @author chenyue7@foxmail.com
@@ -43,35 +52,147 @@ public class EtcdRegistry implements Registry{
     @Override
     public void init(RegistryConfig registryConfig) {
         client = Client.builder()
+                .endpoints(registryConfig.getAddress())
+                .connectTimeout(Duration.ofMillis(registryConfig.getTimeout()))
+                .build();
+        kvClient = client.getKVClient();
+        heartBeat(); // 维护在etcd中的服务节点信息
     }
 
     @Override
     public void register(ServiceMetaInfo serviceMetaInfo) throws Exception {
+        // 创建 Lease 和 KV 客户端
+        Lease leaseClient = client.getLeaseClient();
 
+        // 创建一个30秒的租约
+        long leaseId = leaseClient.grant(30).get().getID();
+
+        // 设置需要存储的键值对
+        String registryKey = ETCD_ROOT_PATH + serviceMetaInfo.getServiceNodeKey();
+        ByteSequence key = ByteSequence.from(registryKey, StandardCharsets.UTF_8);
+        ByteSequence value = ByteSequence.from(JSONUtil.toJsonStr(serviceMetaInfo), StandardCharsets.UTF_8);
+
+        // 将键值对与租约关联起来，并设计过期
+        PutOption putOption = PutOption.builder().withLeaseId(leaseId).build();
+        kvClient.put(key, value, putOption).get();
+        // 添加节点新到本地缓存
+        localRegisterNodeKeySet.add(registryKey);
     }
 
     @Override
     public void unRegister(ServiceMetaInfo serviceMetaInfo) {
-
+        String registerKey = ETCD_ROOT_PATH + serviceMetaInfo.getServiceNodeKey();
+        kvClient.delete(ByteSequence.from(registerKey, StandardCharsets.UTF_8));
+        //也要从本地缓存中移除
+        localRegisterNodeKeySet.remove(registerKey);
     }
 
     @Override
     public List<ServiceMetaInfo> serviceDiscovery(String serviceKey) {
-        return null;
+        // 优先从缓存获取服务
+        List<ServiceMetaInfo> cachedServiceMetaInfoList = registryServiceCache.readCache();
+        if(cachedServiceMetaInfoList != null) {
+            return cachedServiceMetaInfoList;
+        }
+
+        //前缀搜索，结尾一定要加'/'
+        String searchPrefix = ETCD_ROOT_PATH + serviceKey + "/";
+
+        try {
+            GetOption getOption = GetOption.builder().isPrefix(true).build(); // 前缀查询
+            List<KeyValue> keyValues = kvClient.get(ByteSequence.from(searchPrefix, StandardCharsets.UTF_8), getOption)
+                    .get()
+                    .getKvs();
+
+            // 解析服务信息
+            List<ServiceMetaInfo> serviceMetaInfoList = keyValues.stream()
+                    .map(keyValue -> {
+                        String key = keyValue.getKey().toString(StandardCharsets.UTF_8);
+                        // 监听 key 的变化
+                        watch(key);
+                        String value = keyValue.getValue().toString(StandardCharsets.UTF_8);
+                        return JSONUtil.toBean(value, ServiceMetaInfo.class);
+                    })
+                    .collect(Collectors.toList());
+            // 写入服务缓存
+            registryServiceCache.writeCache(serviceMetaInfoList);
+            return serviceMetaInfoList;
+        }catch (Exception e) {
+            throw new RuntimeException("获得服务列表失败", e);
+        }
     }
 
     @Override
     public void heartBeat() {
-
+        // 十秒钟续签一次
+        CronUtil.schedule("*/10 * * * * *", new Task() {
+            @Override
+            public void execute() {
+                for (String key: localRegisterNodeKeySet) {
+                    try {
+                        List<KeyValue> keyValues = kvClient.get(ByteSequence.from(key, StandardCharsets.UTF_8)).get().getKvs();
+                        // 该节点已过期（需要重启节点才能重新注册）
+                        if (CollUtil.isEmpty(keyValues)) {
+                            continue;
+                        }
+                        // 节点未过期，重写注册（相当于续签）
+                        KeyValue keyValue = keyValues.get(0);
+                        String value = keyValue.getValue().toString(StandardCharsets.UTF_8);
+                        ServiceMetaInfo serviceMetaInfo = JSONUtil.toBean(value, ServiceMetaInfo.class);
+                        register(serviceMetaInfo);
+                    }catch (Exception e) {
+                        throw new RuntimeException(key + "续签失败", e);
+                    }
+                }
+            }
+        });
     }
 
+    /**
+     * 监听（消费端）: 清除本地缓存
+     * @param serviceNodeKey
+     */
     @Override
     public void watch(String serviceNodeKey) {
+        Watch watchClient = client.getWatchClient();
+        // 之前未被监听，开启监听
+        boolean newWatch = watchingKeySet.add(serviceNodeKey);
+        if(newWatch) {
+            watchClient.watch(ByteSequence.from(serviceNodeKey, StandardCharsets.UTF_8), response -> {
+                for (WatchEvent event : response.getEvents()) {
+                    switch (event.getEventType()) {
+                        // key 删除时触发
+                        case DELETE:
+                            // 清理注册服务缓存
+                            registryServiceCache.clearCache();
+                            break;
+                        case PUT:
+                        default:
+                            break;
+                    }
+                }
+            });
+        }
 
     }
 
     @Override
     public void destroy() {
+        System.out.println("当前节点下线");
+        for (String key: localRegisterNodeKeySet) {
+            try {
+                kvClient.delete(ByteSequence.from(key, StandardCharsets.UTF_8)).get();
+            } catch (Exception e) {
+                throw new RuntimeException(key + "节点下线失败");
+            }
+        }
 
+        //释放资源
+        if (kvClient != null) {
+            kvClient.close();
+        }
+        if(client != null) {
+            client.close();
+        }
     }
 }
